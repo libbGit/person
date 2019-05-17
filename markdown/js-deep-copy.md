d2925c3a7d088c7552c9879b9c16b452c23e952e
libbGit <824610286@qq.com>
2019-05-17 15:17:41 +0800

    deploy

---
+++ markdown/js-deep-copy.md
@@ -0,0 +1,87 @@
+## 前言 
+对于 *数字*，*boolean* 和 *字符串* 等基本类型 而言，赋值、浅拷贝和深拷贝无意义，因为每次都会在堆中开辟一块新的空间，指向新的地址。
+
+## 一、赋值: 
+指向同一个地址，不拷贝。
+![image](https://image-static.segmentfault.com/266/319/2663191658-5be8fe6ddb083_articlex)
+
+```
+var obj1 = {name:'圆', radius:10, point:{x:0,y:0}};
+var obj2 = obj1;
+ 
+obj2.name = "圆2";  //obj1中的name也会变
+```
+
+## 二、浅拷贝: 
+![image](https://image-static.segmentfault.com/413/426/413426385-5be8fec3bc9e4_articlex)
+
+```
+var obj1 = {name:'圆', radius:10, point:{x:0,y:0}};
+var obj2 = Object.assign({},obj1);
+ 
+obj2.name="圆2"  // obj1.name不会变
+obj2.point.x = 2       //obj1.point.x 改变，因为只拷贝到point一层
+
+同样，解构赋值也是如此
+var obj1 = {name:'圆', radius:10, point:{x:0,y:0}};
+var obj2 = {…obj1}
+```
+
+## 三、深拷贝: 
+![image](https://image-static.segmentfault.com/410/881/4108810455-5be8ff385c57d_articlex)
+
+#### 方法1 
+> JSON.stringify(obj)  先将对象转换为字符串
+>
+> JSON.parse(str)      然后再将字符串转为对象。
+
+```
+var obj1 = {name:'圆', radius:10, point:{x:0,y:0}};
+var obj2 = JSON.stringify(obj1 );
+var obj2 = JSON.parse(obj2);
+ 
+obj2.name = "圆2";  // obj1 不变
+obj2.point.x = 3;     //  obj1 不变
+```
+但JSON.stringify在转换**Date，RegExp对象及function**时会出现问题，同时也会**忽略undefined、function**。
+
+```
+//date 类型
+var o = new Date();
+console.log(o.toString());         //  Mon Nov 06 2017 11:23:35 GMT+0800 (China Standard Time)  本地标准时间
+console.log(JSON.stringify(o));    // "2017-11-06T03:23:35.547Z"  国际标准时间
+```
+因为stringify默认调用的是Object的toJSON方法，所以重写Date的toJSON，然后stringify就是ok的。
+
+```
+Date.prototype.toJSON = function () {
+  return this.toLocaleString();
+}
+console.log(JSON.stringify(o));      // "11/6/2017, 11:23:35 AM"
+```
+
+同理RegExp
+
+```
+//RegExp类型
+r1 = /\d+/;
+console.log(JSON.stringify(r1));           //   {}
+ 
+RegExp.prototype.toJSON = function(){
+return this.toString();
+}
+console.log(JSON.stringify(r1));          //    "/\\d+/" 
+```
+
+#### 方法2
+
+> 类库的方式。jquery,lodash等库
+
+```
+//jquery
+let  y = $.extend(true,{},x)   //第一个参数 必须为true
+
+//lodash库
+let  y = _.cloneDeep(x);
+```
+
---
+++ precache-manifest.bfd6a6ab72d4fd564ce6f5c8720e5c66.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a41ad3f23edd7d633ffb25eddcad7ed",
+    "url": "/person/index.html"
+  },
+  {
+    "revision": "2e5114d1b4af5963cde4",
+    "url": "/person/static/css/main.b0112e80.chunk.css"
+  },
+  {
+    "revision": "004421854ed8360a8fa5",
+    "url": "/person/static/css/vendors~main.742b6d91.chunk.css"
+  },
+  {
+    "revision": "2e5114d1b4af5963cde4",
+    "url": "/person/static/js/main.441620e0.chunk.js"
+  },
+  {
+    "revision": "eeec2df47e524a05c2c2",
+    "url": "/person/static/js/runtime~main.410deb47.js"
+  },
+  {
+    "revision": "004421854ed8360a8fa5",
+    "url": "/person/static/js/vendors~main.f99d0363.chunk.js"
+  },
+  {
+    "revision": "91c28637ce7a9dcd030342db037238e4",
+    "url": "/person/static/media/1.91c28637.jpg"
+  },
+  {
+    "revision": "b8eb2da455cba8d6967ced75d396f146",
+    "url": "/person/static/media/2.b8eb2da4.jpg"
+  },
+  {
+    "revision": "fb83ba8f0e5dc0c1514f059bfc50c439",
+    "url": "/person/static/media/ES6.fb83ba8f.svg"
+  },
+  {
+    "revision": "4d108e0f277b67090d233557f84112dd",
+    "url": "/person/static/media/b03.4d108e0f.jpg"
+  },
+  {
+    "revision": "66fe7128475cbe3beca5814554f0d594",
+    "url": "/person/static/media/canvas.66fe7128.svg"
+  },
+  {
+    "revision": "67fb14f1cc76ce3544bef830ff3720cc",
+    "url": "/person/static/media/iconfont.67fb14f1.eot"
+  },
+  {
+    "revision": "a15186b79e5a9cad3fbdbaa8892f852c",
+    "url": "/person/static/media/iconfont.a15186b7.woff"
+  },
+  {
+    "revision": "b7debe6e0500145d556481d80fa88556",
+    "url": "/person/static/media/iconfont.b7debe6e.svg"
+  },
+  {
+    "revision": "cb5cd9326d675c575c89ad28ad3cb5d0",
+    "url": "/person/static/media/iconfont.cb5cd932.ttf"
+  },
+  {
+    "revision": "797af2785b974e0b29beb41b22044127",
+    "url": "/person/static/media/react.797af278.jpg"
+  },
+  {
+    "revision": "247e948bb0416513b57bb743ecdb754b",
+    "url": "/person/static/media/vue.247e948b.jpg"
+  },
+  {
+    "revision": "296e445ef7d86053c141d8e9ad164177",
+    "url": "/person/static/media/vue.296e445e.svg"
+  }
+]);
